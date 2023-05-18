e1f451fd313ee0a687cb6bc8086ab45123178683
Vidroid <33814394+Vidroid@users.noreply.github.com>
2023-05-18 20:49:03 +0300

    Dice
    Added dice message commen when they sended

--- main.py
+++ main.py
@@ -1,84 +1,110 @@
-#!venv/bin/python
-import logging
-import conf
+try:
 
-from aiogram import Bot, Dispatcher, executor, types
+    # !venv/bin/python
+    import logging
+    import conf
 
-# Объект бота
-bot = Bot(token=conf.TOKEN, parse_mode='HTML')
-# Диспетчер для бота
-dp = Dispatcher(bot)
-# Включаем логирование, чтобы не пропустить важные сообщения
-logging.basicConfig(level=logging.INFO)
+    from aiogram import Bot, Dispatcher, executor, types
+
+    # Объект бота
+    bot = Bot(token=conf.TOKEN, parse_mode='HTML')
+    # Диспетчер для бота
+    dp = Dispatcher(bot)
+    # Включаем логирование, чтобы не пропустить важные сообщения
+    logging.basicConfig(filename='tbot.log', encoding='utf-8', level=logging.DEBUG)
+    # logging.basicConfig(level=logging.INFO)
+    logging.debug('obj init')
 
 
-# Хэндлер на команду /test1
-@dp.message_handler()
-async def cmd_test1(message: types.Message):
+    # Хэндлер на команду /test1
+    @dp.message_handler()
+    async def cmd_test1(message: types.Message):
+        logging.debug('into handler')
+        if message.chat.id == conf.PIN:
+            logging.debug('into if')
+            # -1001579740736
+            if message.text.lower() == 'pcast h':
+                if message.reply_to_message:
+                    msg = await bot.send_message(conf.HIGH, message.reply_to_message.text)
+                    await bot.pin_chat_message(conf.HIGH, msg.message_id)
+                    await bot.send_message(conf.PIN, 'Done to H')
 
-    if message.chat.id == conf.PIN:
-        #-1001579740736
-        if message.text.lower() == 'pcast h':
-            if message.reply_to_message:
-                msg = await bot.send_message(conf.HIGH, message.reply_to_message.text)
-                await bot.pin_chat_message(conf.HIGH, msg.message_id)
-                await bot.send_message(conf.PIN, 'Done to H')
+            if message.text.lower() == 'pcast w':
+                if message.reply_to_message:
+                    msg = await bot.send_message(conf.LAMP, message.reply_to_message.text)
+                    await bot.pin_chat_message(conf.LAMP, msg.message_id)
+                    await bot.send_message(conf.PIN, 'Done to Lamp')
 
-        if message.text.lower() == 'pcast w':
-            if message.reply_to_message:
-                msg = await bot.send_message(conf.LAMP, message.reply_to_message.text)
-                await bot.pin_chat_message(conf.LAMP, msg.message_id)
-                await bot.send_message(conf.PIN, 'Done to Lamp')
+            if message.text.lower() == 'pcast t':
+                if message.reply_to_message:
+                    msg = await bot.send_message(conf.TOP, message.reply_to_message.text)
+                    await bot.pin_chat_message(conf.TOP, msg.message_id)
+                    await bot.send_message(conf.PIN, 'Done to T')
 
-        if message.text.lower() == 'pcast t':
-            if message.reply_to_message:
-                msg = await bot.send_message(conf.TOP, message.reply_to_message.text)
-                await bot.pin_chat_message(conf.TOP, msg.message_id)
-                await bot.send_message(conf.PIN, 'Done to T')
+            if message.text.lower() == 'pcast a':
+                if message.reply_to_message:
+                    await bot.unpin_all_chat_messages(conf.LAMP)
+                    msg = await bot.send_message(conf.LAMP, message.reply_to_message.text)
+                    await bot.pin_chat_message(conf.LAMP, msg.message_id)
+                    await bot.unpin_all_chat_messages(conf.TOP)
+                    msg2 = await bot.send_message(conf.TOP, message.reply_to_message.text)
+                    await bot.pin_chat_message(conf.TOP, msg2.message_id)
+                    await bot.unpin_all_chat_messages(conf.HIGH)
+                    msg3 = await bot.send_message(conf.HIGH, message.reply_to_message.text)
+                    await bot.pin_chat_message(conf.HIGH, msg3.message_id)
+                    await bot.send_message(conf.PIN, 'Done to ALL')
 
-        if message.text.lower() == 'pcast a':
-            if message.reply_to_message:
-                await bot.unpin_all_chat_messages(conf.LAMP)
-                msg = await bot.send_message(conf.LAMP, message.reply_to_message.text)
-                await bot.pin_chat_message(conf.LAMP, msg.message_id)
-                await bot.unpin_all_chat_messages(conf.TOP)
-                msg2 = await bot.send_message(conf.TOP, message.reply_to_message.text)
-                await bot.pin_chat_message(conf.TOP, msg2.message_id)
-                await bot.unpin_all_chat_messages(conf.HIGH)
-                msg3 = await bot.send_message(conf.HIGH, message.reply_to_message.text)
-                await bot.pin_chat_message(conf.HIGH, msg3.message_id)
-                await bot.send_message(conf.PIN, 'Done to ALL')
+            if message.text.lower() == 'ncast h':
+                if message.reply_to_message:
+                    msg = await bot.send_message(conf.HIGH, message.reply_to_message.text)
+                    await bot.send_message(conf.PIN, 'Done to H')
 
-        if message.text.lower() == 'ncast h':
-            if message.reply_to_message:
-                msg = await bot.send_message(conf.HIGH, message.reply_to_message.text)
-                await bot.send_message(conf.PIN, 'Done to H')
+            if message.text.lower() == 'ncast w':
+                if message.reply_to_message:
+                    msg = await bot.send_message(conf.LAMP, message.reply_to_message.text)
+                    await bot.send_message(conf.PIN, 'Done to Lamp')
 
-        if message.text.lower() == 'ncast w':
-            if message.reply_to_message:
-                msg = await bot.send_message(conf.LAMP, message.reply_to_message.text)
-                await bot.send_message(conf.PIN, 'Done to Lamp')
+            if message.text.lower() == 'ncast t':
+                if message.reply_to_message:
+                    msg = await bot.send_message(conf.TOP, message.reply_to_message.text)
+                    await bot.send_message(conf.PIN, 'Done to T')
 
-        if message.text.lower() == 'ncast t':
-            if message.reply_to_message:
-                msg = await bot.send_message(conf.TOP, message.reply_to_message.text)
-                await bot.send_message(conf.PIN, 'Done to T')
+            if message.text.lower() == 'ncast a':
+                if message.reply_to_message:
+                    msg = await bot.send_message(conf.LAMP, message.reply_to_message.text)
+                    msg2 = await bot.send_message(conf.TOP, message.reply_to_message.text)
+                    msg3 = await bot.send_message(conf.HIGH, message.reply_to_message.text)
+                    await bot.send_message(conf.PIN, 'Done to ALL')
 
-        if message.text.lower() == 'ncast a':
-            if message.reply_to_message:
-                msg = await bot.send_message(conf.LAMP, message.reply_to_message.text)
-                msg2 = await bot.send_message(conf.TOP, message.reply_to_message.text)
-                msg3 = await bot.send_message(conf.HIGH, message.reply_to_message.text)
-                await bot.send_message(conf.PIN, 'Done to ALL')
+        if message.from_user.id == conf.ME:
+            if message.text.lower() == 'дайид':
+                await bot.send_message(message.chat.id, message.chat.id)
 
-    if message.from_user.id == conf.ME:
-        if message.text.lower() == 'дайид':
-            await bot.send_message(message.chat.id, message.chat.id)
+        if message.from_user.id == conf.ME:
+            if message.text.lower() == 'ало':
+                await bot.send_message(message.chat.id, 'who-ем по лбу не дало?')
 
-    if message.from_user.id == conf.ME:
-        if message.text.lower() == 'алтарь':
-            await bot.send_dice(message.chat.id)
+        if message.from_user.id == conf.ME:
+            if message.text.lower() == 'алтарь':
+                msg = await bot.send_dice(message.chat.id)
+                if msg.dice.value == 1:
+                    await bot.send_message(message.chat.id, 'Иса')
+                elif msg.dice.value == 2:
+                    await bot.send_message(message.chat.id, 'Гебо')
+                elif msg.dice.value == 3:
+                    await bot.send_message(message.chat.id, 'Исс')
+                elif msg.dice.value == 4:
+                    await bot.send_message(message.chat.id, 'Дагаз')
+                elif msg.dice.value == 5:
+                    await bot.send_message(message.chat.id, 'Тир')
+                elif msg.dice.value == 6:
+                    await bot.send_message(message.chat.id, 'Ну хуууууй знает. Тут Эйви. Мб лучше Тир?')
 
-if __name__ == "__main__":
-    # Запуск бота
-    executor.start_polling(dp, skip_updates=True)
+    if __name__ == "__main__":
+        # Запуск бота
+        logging.debug('into exec')
+        executor.start_polling(dp, skip_updates=True)
+        logging.debug('outto exec')
+except Exception as inst:
+
+    logging.debug(type(inst), inst.args, inst)
